daf0b714ab9910e0dc98a1679a4ed97d1bbe5554
Uszczi <mateuszpapuga24@gmail.com>
2022-02-04 10:07:55 +0100

    add autoscript for qtile

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
 
-set-random-wallpaper
-xrandr --output "eDP1"  --right-of "HDMI2" &
-# /bin/emacs  --daemon &
-guake &
-sudo systemctl start docker.service
+os=$(cat /etc/*-release | rg ^ID= | awk -F '=' '{print $2}')
+echo $os
 
+
+if [[ "$os" == "ubuntu" ]]; then
+    compton &
+elif [[ "$os" == "arch" ]]; then
+    set-random-wallpaper
+    xrandr --output "eDP1"  --right-of "HDMI2" &
+    # /bin/emacs  --daemon &
+    guake &
+    sudo systemctl start docker.service
+fi
