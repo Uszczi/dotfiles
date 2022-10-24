3fbe7a952a18157b663b4db801b13deaf044fd9d
Uszczi <mateuszpapuga24@gmail.com>
2022-10-24 14:14:04 +0200

    clean up

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,4 @@
-vim.g.leader = " "
-
-local fn = vim.fn
-local execute = vim.api.nvim_command
-
-local install_path = fn.stdpath("data") .. "/site/pack/packer/opt/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-    execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
-end
-
-vim.cmd [[packadd packer.nvim]]
-vim.cmd "autocmd BufWritePost uszczi/packer.lua PackerCompile"
-
-require("uszczi.plugins_list")
+require("uszczi.plugins")
 require("uszczi.auto-run-command")
 require("uszczi.scrapy-run")
 require("uszczi.globals")
@@ -38,7 +25,6 @@ vim.cmd "au TextYankPost * lua vim.highlight.on_yank {on_visual = false}"
 
 require "fidget".setup {}
 
--- vim.cmd("source " .. "~/dotfiles/nvim/" .. "lua/uszczi/colors/gruvbox.vim")
 require("uszczi.colors.tokyonight")
 
 require("nvim-autopairs").setup {}
