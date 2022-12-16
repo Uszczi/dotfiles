a77a509a71ec13847c6987f8f76842064c637111
Uszczi <mateuszpapuga24@gmail.com>
2022-12-16 17:29:11 +0100

    add dotfiles bin to path

--- fish/config.fish
+++ fish/config.fish
@@ -16,6 +16,7 @@ fish_add_path ~/.local/bin
 fish_add_path ~/.cargo/bin
 fish_add_path ~/neovim/bin
 fish_add_path ~/dotfiles/path-utils
+fish_add_path ~/dotfiles/dotfiles-private/bin
 fish_add_path /usr/local/go/bin
 fish_add_path ~/BurpSuiteCommunity
 fish_add_path ~/src/xpdf-tools-linux-4.04/bin64
