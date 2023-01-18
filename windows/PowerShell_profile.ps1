653c7eb5e39c5386bd5e9ca4260e4e596eed3587
Mateusz Papuga <mateuszpapuga24@gmail.com>
2023-01-18 12:02:13 +0100

    add cd aliases

--- windows/PowerShell_profile.ps1
+++ windows/PowerShell_profile.ps1
@@ -1,3 +1,9 @@
 oh-my-posh init pwsh --config ~/dotfiles/windows/oh-my-posh-theme.omp.json | Invoke-Expression
 
 Set-Alias -Name vi -Value nvim
+
+
+function goUpDir { Set-Location ".." }
+function goUpUpDir { Set-Location "../.." }
+Set-Alias -Name .. -Value goUpDir
+Set-Alias -Name ... -Value goUpUpDir
