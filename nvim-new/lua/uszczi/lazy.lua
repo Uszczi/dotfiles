b26143320efb1e3fe85a1974c121904347984838
Mateusz Papuga <mateuszpapuga24@gmail.com>
2023-01-29 15:34:08 +0100

    add neoformat

--- nvim-new/lua/uszczi/lazy.lua
+++ nvim-new/lua/uszczi/lazy.lua
@@ -1,36 +1,37 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+    vim.fn.system(
+        {
+            "git",
+            "clone",
+            "--filter=blob:none",
+            "https://github.com/folke/lazy.nvim.git",
+            "--branch=stable",
+            lazypath
+        }
+    )
 end
 
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-	-- Theme
-	{
-		"navarasu/onedark.nvim",
-		init = function()
-			vim.cmd([[colorscheme onedark]])
-		end,
-	},
-
-	-- Navigation
-	"ThePrimeagen/harpoon",
-	"nvim-telescope/telescope.nvim",
-
-	-- Dependencies
-	"nvim-lua/plenary.nvim",
-
-	-- Formating
-	"sbdchd/neoformat",
-
-	-- Debug
-	{ "dstein64/vim-startuptime", cmd = "StartupTime" },
-}, {})
+require("lazy").setup(
+    {
+        -- Theme
+        {
+            "navarasu/onedark.nvim",
+            init = function()
+                vim.cmd([[colorscheme onedark]])
+            end
+        },
+        -- Navigation
+        "ThePrimeagen/harpoon",
+        "nvim-telescope/telescope.nvim",
+        -- Dependencies
+        "nvim-lua/plenary.nvim",
+        -- Formating
+        "sbdchd/neoformat",
+        -- Debug
+        {"dstein64/vim-startuptime", cmd = "StartupTime"}
+    },
+    {}
+)
