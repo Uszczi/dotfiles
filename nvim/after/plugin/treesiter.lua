1a7a5fba2cf7b0269e4814d5ab419b17247c18c2
Uszczi <mateuszpapuga24@gmail.com>
2023-01-30 17:41:47 +0100

    add json treesiter parser

--- nvim/after/plugin/treesiter.lua
+++ nvim/after/plugin/treesiter.lua
@@ -4,7 +4,7 @@ end
 
 require "nvim-treesitter.configs".setup {
     -- A list of parser names, or "all" (the four listed parsers should always be installed)
-    ensure_installed = {"python", "lua", "vim", "help"},
+    ensure_installed = {"python", "lua", "vim", "help", "json"},
     highlight = {
         enable = true
     }
