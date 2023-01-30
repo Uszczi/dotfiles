b7655ace9df8675a8aca28cd548665d106934de3
Uszczi <mateuszpapuga24@gmail.com>
2023-01-30 16:42:07 +0100

    :adhesive_bandage: Make command silent

--- nvim/after/plugin/neogit.lua
+++ nvim/after/plugin/neogit.lua
@@ -2,4 +2,4 @@ local neogit = require("neogit")
 
 neogit.setup({})
 
-vim.keymap.set("n", "<leader>gg", ":lua require'neogit'.open({})<CR>")
+vim.keymap.set("n", "<leader>gg", ":lua require'neogit'.open({})<CR>", {silent = true})
