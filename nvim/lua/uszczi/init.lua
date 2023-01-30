150ca4759d84f21b9a51932744a31ba36efc6fa5
Uszczi <mateuszpapuga24@gmail.com>
2023-01-30 16:43:08 +0100

    🚚 create lualine

--- nvim/lua/uszczi/init.lua
+++ nvim/lua/uszczi/init.lua
@@ -3,17 +3,6 @@ require "uszczi.lazy"
 
 require "uszczi.remap"
 
--- Set lualine as statusline
--- See `:help lualine.txt`
-require("lualine").setup {
-    options = {
-        icons_enabled = false,
-        theme = "onedark",
-        component_separators = "|",
-        section_separators = ""
-    }
-}
-
 -- Enable Comment.nvim
 require("Comment").setup()
 
