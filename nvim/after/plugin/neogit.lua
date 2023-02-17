a62114547cce2526bde88483d07b4a1cfe414126
Uszczi <mateuszpapuga24@gmail.com>
2023-02-17 14:14:21 +0100

    fix neogit in subdirectory

--- nvim/after/plugin/neogit.lua
+++ nvim/after/plugin/neogit.lua
@@ -2,4 +2,15 @@ local neogit = require("neogit")
 
 neogit.setup({})
 
-vim.keymap.set("n", "<leader>gg", ":lua require'neogit'.open({})<CR>", {silent = true})
+local function open_neogit()
+    local path = vim.loop.cwd() .. "/.git"
+    local ok, _ = vim.loop.fs_stat(path)
+
+    if ok then
+        neogit.open({})
+    else
+        neogit.open({cwd = ".."})
+    end
+end
+
+vim.keymap.set("n", "<leader>gg", open_neogit)
