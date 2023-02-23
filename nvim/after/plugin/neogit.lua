b84df9b4b0e2b6deb32f44b6e74b61dc0850cbed
Uszczi <mateuszpapuga24@gmail.com>
2023-02-23 13:39:18 +0100

    remove fix for neogit

--- nvim/after/plugin/neogit.lua
+++ nvim/after/plugin/neogit.lua
@@ -2,15 +2,4 @@ local neogit = require("neogit")
 
 neogit.setup({})
 
-local function open_neogit()
-    local path = vim.loop.cwd() .. "/.git"
-    local ok, _ = vim.loop.fs_stat(path)
-
-    if ok then
-        neogit.open({})
-    else
-        neogit.open({cwd = ".."})
-    end
-end
-
-vim.keymap.set("n", "<leader>gg", open_neogit)
+vim.keymap.set("n", "<leader>gg", require "neogit".open)
