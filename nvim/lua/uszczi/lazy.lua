648124c80c7d6e2d46f1ce47dee456878c861c15
Uszczi <mateuszpapuga24@gmail.com>
2023-03-15 13:06:29 +0100

    update telescope and and which-key

--- nvim/lua/uszczi/lazy.lua
+++ nvim/lua/uszczi/lazy.lua
@@ -46,6 +46,14 @@ require("lazy").setup(
         -- Navigation
         "ThePrimeagen/harpoon",
         "nvim-tree/nvim-tree.lua",
+        {
+            "folke/which-key.nvim",
+            config = function()
+                vim.o.timeout = true
+                vim.o.timeoutlen = 300
+                require("which-key").setup({})
+            end
+        },
         -- Dependencies
         "nvim-lua/plenary.nvim",
         -- Formating
@@ -65,6 +73,17 @@ require("lazy").setup(
         "mhinz/vim-signify",
         "tpope/vim-fugitive",
         "sindrets/diffview.nvim",
+        {
+            "NWVi/octo.nvim",
+            branch = "config-review-use-local-fs",
+            init = function()
+                require "octo".setup(
+                    {
+                        use_local_fs = true
+                    }
+                )
+            end
+        },
         -- LSP
         "neovim/nvim-lspconfig",
         "williamboman/mason.nvim",
@@ -84,7 +103,8 @@ require("lazy").setup(
         -- TPope
         "tpope/vim-sleuth", -- This plugin automatically adjusts 'shiftwidth' and 'expandtab' heuristically based on the current file
         "tpope/vim-repeat",
-        {dir = "$HOME/projects/nvim-scrapy-debug"}
+        -- {dir = "$HOME/projects/nvim-scrapy-debug"}
+        "Uszczi/nvim-scrapy-debug"
     },
     {}
 )
