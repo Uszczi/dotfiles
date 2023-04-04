6b6a2e054477fd0f0bfc898258d61dec68b8b76c
mat <mateuszpapuga24@gmail.com>
2023-04-04 22:59:42 +0200

    add unescape

--- nvim/lua/uszczi/init.lua
+++ nvim/lua/uszczi/init.lua
@@ -4,36 +4,55 @@ require "uszczi.lazy"
 require "uszczi.remap"
 require "uszczi.globals"
 
-local hex_to_char = function(x)
-    return string.char(tonumber(x, 16))
+require "uszczi.unescape"
+
+local term = require "harpoon.term"
+
+local function getBufferText()
+    return vim.api.nvim_buf_get_lines(0, 0, -1, false)
 end
 
-local unescape = function(url)
-    return url:gsub("%%(%x%x)", hex_to_char)
-end
-
-local function print_selected_text()
+local function getSelectedText()
     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<esc>", true, false, true), "x", false)
-
     local _, csrow, cscol, _ = unpack(vim.fn.getpos("'<"))
     local _, cerow, cecol, _ = unpack(vim.fn.getpos("'>"))
 
+    local text
     if cecol > 10000 then
-        local text = vim.api.nvim_buf_get_text(0, csrow - 1, cscol - 1, cerow - 1, cecol - 1, {})
-        text = unescape(text[1])
-        vim.api.nvim_buf_set_lines(0, csrow - 1, csrow, false, {text})
+        text = vim.api.nvim_buf_get_text(0, csrow - 1, cscol - 1, cerow - 1, cecol - 1, {})
     else
-        local text = vim.api.nvim_buf_get_text(0, csrow - 1, cscol - 1, cerow - 1, cecol, {})
-        text = unescape(text[1])
-        vim.api.nvim_buf_set_text(0, csrow - 1, cscol - 1, cerow - 1, cecol, {text})
+        text = vim.api.nvim_buf_get_text(0, csrow - 1, cscol - 1, cerow - 1, cecol, {})
     end
+
+    return text, csrow, cscol, cerow, cecol
 end
+
+local function sendToTerminalSelection(idx)
+    local text = getSelectedText()
+    term.sendCommand(idx, table.concat(text, "\n") .. "\n")
+end
+
+local function sendToTerminalBuffer(idx)
+    local text = getBufferText()
+    term.sendCommand(idx, table.concat(text, "\n") .. "\n")
+end
+
 
 vim.keymap.set(
     {"n", "v"},
-    "<leader>ru",
+    "<leader>rp",
     function()
-        print_selected_text()
+        sendToTerminalSelection(2)
     end,
     {silent = false}
 )
+
+vim.keymap.set(
+    {"n", "v"},
+    "<leader>rP",
+    function()
+        sendToTerminalBuffer(2)
+    end,
+    {silent = false}
+)
+
