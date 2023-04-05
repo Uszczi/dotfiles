1030ea4a47f83a6773694dd2f629dc46b6668744
mat <mateuszpapuga24@gmail.com>
2023-04-05 12:55:44 +0200

    experiment with python execute

--- nvim/lua/uszczi/init.lua
+++ nvim/lua/uszczi/init.lua
@@ -12,6 +12,25 @@ local function getBufferText()
     return vim.api.nvim_buf_get_lines(0, 0, -1, false)
 end
 
+local function filterText(text)
+    local filtered = {}
+
+    for i, part in ipairs(text) do
+        if part ~= "" then
+            table.insert(filtered, part)
+        else
+            if text[i + 1] == "" then
+                table.insert(filtered, part)
+            end
+        end
+    end
+
+    return filtered
+end
+
+
+
+
 local function getSelectedText()
     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<esc>", true, false, true), "x", false)
     local _, csrow, cscol, _ = unpack(vim.fn.getpos("'<"))
@@ -29,12 +48,13 @@ end
 
 local function sendToTerminalSelection(idx)
     local text = getSelectedText()
-    term.sendCommand(idx, table.concat(text, "\n") .. "\n")
+    text = filterText(text)
+    term.sendCommand(idx, table.concat(text, "\n") .. "\n\n")
 end
 
 local function sendToTerminalBuffer(idx)
     local text = getBufferText()
-    term.sendCommand(idx, table.concat(text, "\n") .. "\n")
+    term.sendCommand(idx, table.concat(text, "\n") .. "\n\n")
 end
 
 
