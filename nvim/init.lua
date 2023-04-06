708c1604c97ac73519f7a8de2ba0a65e213fe119
mat <mateuszpapuga24@gmail.com>
2023-04-06 18:50:53 +0200

    update nvim-tree

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,28 +6,13 @@ require "uszczi"
 vim.opt.termguicolors = true
 
 -- empty setup using defaults
-require("nvim-tree").setup()
-
--- OR setup with some options
-require("nvim-tree").setup(
-    {
-        sort_by = "case_sensitive",
-        view = {
-            width = 30,
-            mappings = {
-                list = {
-                    {key = "u", action = "dir_up"}
-                }
-            }
-        },
-        renderer = {
-            group_empty = true
-        },
-        filters = {
-            dotfiles = true
-        }
+require("nvim-tree").setup({
+    reload_on_bufenter = true,
+      update_focused_file = {
+        enable = true,
     }
-)
+})
+
 
 vim.keymap.set("n", "<leader>op", ":NvimTreeFindFileToggle<CR>", {silent = false})
 
