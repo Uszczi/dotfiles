8db25f91ed5bbb1f4b4afb446564a9d15b9ee9ad
mat <mateuszpapuga24@gmail.com>
2023-07-13 12:45:58 +0200

    remove custom dapui

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -16,40 +16,7 @@ dap.adapters.generic_remote = function(callback, config)
     callback({type = "server", host = "127.0.0.1", port = 5678})
 end
 
-require("dapui").setup(
-    {
-        layouts = {
-            {
-                elements = {
-                    {
-                        id = "scopes",
-                        size = 0.25 -- Can be float or integer > 1
-                    },
-                    {id = "breakpoints", size = 0.25},
-                    {id = "stacks", size = 0.25},
-                    {id = "watches", size = 0.25}
-                },
-                size = 40,
-                position = "left" -- Can be "left" or "right"
-            },
-            {
-                elements = {
-                    "repl",
-                    "console"
-                },
-                size = 10,
-                position = "bottom" -- Can be "bottom" or "top"
-            },
-            {
-                elements = {
-                    "console"
-                },
-                size = 20,
-                position = "bottom" -- Can be "bottom" or "top"
-            }
-        }
-    }
-)
+require("dapui").setup({})
 
 vim.keymap.set({"n", "v"}, "<leader>dw", ":lua require'dapui'.eval()<CR>", {silent = true})
 vim.keymap.set(
