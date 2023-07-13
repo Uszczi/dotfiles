0cc46f095bf16257a7ec2845719a3fe0c72a7bef
mat <mateuszpapuga24@gmail.com>
2023-07-13 12:47:40 +0200

    enable incremental treesiter selection

--- nvim/after/plugin/treesiter.lua
+++ nvim/after/plugin/treesiter.lua
@@ -7,5 +7,14 @@ require "nvim-treesitter.configs".setup {
     ensure_installed = {"python", "lua", "vim", "help", "json"},
     highlight = {
         enable = true
+    },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm"
+        }
     }
 }
