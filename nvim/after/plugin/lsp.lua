0700ad2ec828abe93dadab84fcfcd5828de83fff
Mateusz Papuga <mateuszpapuga24@gmail.com>
2023-09-12 18:35:05 +0200

    feat: remove mason ensure_installed

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -62,9 +62,7 @@ capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 require("mason").setup()
 
 local mason_lspconfig = require "mason-lspconfig"
-mason_lspconfig.setup {
-    ensure_installed = vim.tbl_keys(servers)
-}
+mason_lspconfig.setup {}
 
 mason_lspconfig.setup_handlers {
     function(server_name)
@@ -75,10 +73,3 @@ mason_lspconfig.setup_handlers {
         }
     end
 }
--- require "lspconfig".ruff_lsp.setup {
---     init_options = {
---         settings = {
---             args = {"--ignore", "E501"}
---         }
---     }
--- }
