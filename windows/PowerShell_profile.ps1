2273d924bc46614b84fd1a3ed2086665cb41e514
Mateusz Papuga <mateuszpapuga24@gmail.com>
2023-09-12 19:57:08 +0200

    feat: merge windows

--- windows/PowerShell_profile.ps1
+++ windows/PowerShell_profile.ps1
@@ -1,5 +1,3 @@
-oh-my-posh init pwsh --config ~/dotfiles/windows/oh-my-posh-theme.omp.json | Invoke-Expression
-
 Set-PSReadLineOption -EditMode Emacs   
 
 Set-Alias -Name vi -Value nvim
@@ -10,4 +8,21 @@ function goUpUpDir { Set-Location "../.." }
 Set-Alias -Name .. -Value goUpDir
 Set-Alias -Name ... -Value goUpUpDir
 
-Set-Alias -Name python3 -Value python
+Set-Alias -Name python3 -Value py.exe
+Set-Alias -Name python -Value py.exe
+Set-Alias -Name python310 -Value C:\Users\mateu\AppData\Local\Programs\Python\Python310\python.exe
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+if (Get-Command "oh-my-posh.exe" -ErrorAction SilentlyContinue) 
+{ 
+    oh-my-posh init pwsh  --config "$env:HOME\dotfiles\windows\oh-my-posh-theme.omp.json" | Invoke-Expression
+}
--- windows/autostart.ahk
+++ windows/autostart.ahk
@@ -13,6 +13,10 @@
 RunWait('wt.exe -p "Windows PowerShell"')
 }
 
+!Enter:: {
+RunWait('wt.exe -p "Windows PowerShell"')
+}
+
 #+Enter:: {
   RunWait("wt C:\Windows\system32\wsl.exe -d Ubuntu")
 }
