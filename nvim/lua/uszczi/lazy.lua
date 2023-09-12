575bdcd911a275fb1ffc9d4e8cb8e75bf17739cc
Mateusz Papuga <mateuszpapuga24@gmail.com>
2023-09-12 19:49:25 +0200

    chore: clean

--- nvim/lua/uszczi/lazy.lua
+++ nvim/lua/uszczi/lazy.lua
@@ -23,27 +23,6 @@ require("lazy").setup(
       end,
     },
         {
-          "nvim-neorg/neorg",
-          build = ":Neorg sync-parsers",
-          dependencies = { "nvim-lua/plenary.nvim" },
-          config = function()
-            require("neorg").setup {
-              load = {
-                ["core.defaults"] = {}, -- Loads default behaviour
-                ["core.concealer"] = {}, -- Adds pretty icons to your documents
-                ["core.dirman"] = { -- Manages Neorg workspaces
-                  config = {
-                    workspaces = {
-                      notes = "~/notes",
-                    },
-                    default_workspace = "notes",
-                  },
-                },
-              },
-            }
-          end,
-        },
-        {
             "folke/trouble.nvim",
             dependencies = {"nvim-tree/nvim-web-devicons"},
             opts = {}
