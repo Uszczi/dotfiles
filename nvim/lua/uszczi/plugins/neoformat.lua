4e53eb2ba68013742363c5ac0dba09ddf9a72aa1
Mateusz Papuga <mateuszpapuga24@gmail.com>
2023-10-09 21:52:38 +0200

    feat: add automatic formatters

--- nvim/lua/uszczi/plugins/neoformat.lua
+++ nvim/lua/uszczi/plugins/neoformat.lua
@@ -1,12 +1,13 @@
 vim.g.neoformat_enabled_lua = {"luafmt"}
 vim.g.neoformat_enabled_python = {"black"}
+vim.g.neoformat_enabled_javascript = {"prettierd"}
 
 vim.keymap.set("n", "<leader>cc", ":Neoformat<CR>")
 
 vim.api.nvim_create_autocmd(
     "BufWritePre",
     {
-        pattern = "*lua",
+        pattern = "*js,*lua,*py",
         callback = function()
             vim.cmd [[ Neoformat ]]
         end
