7d08c1933289443306eb57e4b3a225a0e30d6b8c
Mateusz Papuga <mateuszpapuga24@gmail.com>
2023-10-16 22:02:02 +0200

    feat: simplify install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,23 +3,22 @@
 sudo apt -y update & sudo apt -y upgrade
 sudo apt-get -y update & sudo apt-get -y upgrade
 
-sudo apt install fish exa ripgrep fd-find flameshot
+sudo apt install fish exa ripgrep fd-find flameshot blueman tmux
 
-echo "Installing oh-my-fish."
 if [[ ! -d "$HOME/.local/share/omf" ]]; then
     curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
-else
-    echo "oh-my-fish exists."
-    echo ""
 fi
 
-echo "Installing kitty."
 if [[ ! -d "$HOME/.local/kitty.app" ]]; then
     curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-else
-    echo "kitty exists."
-    echo ""
 fi
 
-echo "Computer should be ready."
+if [[ ! -d "$HOME/.local/share/fnm" ]]; then
+    curl -fsSL https://fnm.vercel.app/install | bash
+fi
+
+# Cargo and rust has to be run by hand
+echo "Rust and Cargo has to be install by hand"
+echo "curl https://sh.rustup.rs -sSf | sh"
+
 
