6e326221a06d430a35108a3c3725ca28c009f8d2
Mateusz Papuga <mateusz.papuga@stxnext.pl>
2024-10-17 11:33:37 +0200

    update oil.nvim

--- nvim/after/plugin/oil.lua
+++ nvim/after/plugin/oil.lua
@@ -1,3 +1,16 @@
-require("oil").setup()
+local oil = require("oil")
 
-vim.api.nvim_set_keymap("n", "<leader>fv", "<CMD>Oil<CR>", {})
+oil.setup(
+    {
+        view_options = {show_hidden = true},
+        keymaps = {
+            ["<C-v>"] = {"actions.select", opts = {vertical = true}, desc = "Open the entry in a vertical split"},
+            ["<C-x>"] = {"actions.select", opts = {horizontal = true}, desc = "Open the entry in a horizontal split"}
+        }
+    }
+)
+
+vim.keymap.set("n", "<leader>fv", "<CMD>Oil<CR>", {})
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", {})
+vim.keymap.set("n", "<leader>-", oil.toggle_float, {})
+vim.keymap.set("n", "<leader>i", oil.toggle_float, {})
