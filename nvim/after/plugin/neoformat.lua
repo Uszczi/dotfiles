c7dfdea33eb58af95383fbbee693bb1d78c844e2
Mateusz Papuga <mateuszpapuga24@gmail.com>
2025-12-14 20:59:47 -0800

    🐛 fix neoformat

--- nvim/after/plugin/neoformat.lua
+++ nvim/after/plugin/neoformat.lua
@@ -1,13 +1,14 @@
 -- vim.g.neoformat_enabled_lua = {"luafmt"}
 vim.g.neoformat_enabled_python = { "black" }
-vim.g.neoformat_enabled_javascript = { "prettierd" }
+vim.g.neoformat_enabled_javascript = { "prettierd", "prettier" }
+vim.g.neoformat_enabled_typescript = { "prettierd", "prettier" }
 
 vim.g.neoformat_verbose = 0
 
 vim.keymap.set("n", "<leader>cc", ":Neoformat<CR>")
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*js,*lua,*py",
+	pattern = "*.js,*.jsx,*.ts,*.tsx,*.lua,*.py",
 	callback = function()
 		vim.cmd([[ Neoformat ]])
 	end,
