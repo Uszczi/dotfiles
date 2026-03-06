dcbbb54c4c33bc0e7d869fdec1b6e537aca3c8c9
Mateusz Papuga <mateuszpapuga24@gmail.com>
2026-03-05 22:21:05 -0700

    ThePrimeagen King

--- nvim/lua/uszczi/plugins/lsp.lua
+++ nvim/lua/uszczi/plugins/lsp.lua
@@ -1,31 +1,145 @@
 return {
   "neovim/nvim-lspconfig",
-  lazy = false,
   dependencies = {
+    "stevearc/conform.nvim",
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
-    "folke/neodev.nvim",
-    { "j-hui/fidget.nvim", opts = {} },
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/nvim-cmp",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "j-hui/fidget.nvim",
   },
-  keys = {
-    { "<leader>rn", vim.lsp.buf.rename, desc = "LSP: [R]e[n]ame" },
-    { "<leader>ca", vim.lsp.buf.code_action, desc = "LSP: [C]ode [A]ction" },
-    { "gd", function() require("telescope.builtin").lsp_definitions() end, desc = "LSP: [G]oto [D]efinition" },
-    { "gr", function() require("telescope.builtin").lsp_references() end, desc = "LSP: [G]oto [R]eferences" },
-    { "gI", vim.lsp.buf.implementation, desc = "LSP: [G]oto [I]mplementation" },
-    { "<leader>D", vim.lsp.buf.type_definition, desc = "LSP: Type [D]efinition" },
-    {
-      "<leader>ds",
-      function() require("telescope.builtin").lsp_document_symbols() end,
-      desc = "LSP: [D]ocument [S]ymbols",
-    },
-    { "K", vim.lsp.buf.hover, desc = "LSP: Hover Documentation" },
-    { "<C-k>", vim.lsp.buf.signature_help, desc = "LSP: Signature Documentation" },
-    { "gD", vim.lsp.buf.declaration, desc = "LSP: [G]oto [D]eclaration" },
-  },
+
   config = function()
-    require("neodev").setup()
+    require("conform").setup({
+      formatters_by_ft = {},
+    })
+    local cmp = require("cmp")
+    local cmp_lsp = require("cmp_nvim_lsp")
+    local capabilities =
+      vim.tbl_deep_extend("force", {}, vim.lsp.protocol.make_client_capabilities(), cmp_lsp.default_capabilities())
+
+    require("fidget").setup({})
     require("mason").setup()
-    require("mason-lspconfig").setup()
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        -- Add for python :)
+        "lua_ls",
+        "rust_analyzer",
+        "gopls",
+        "vtsls",
+        "tailwindcss",
+      },
+      handlers = {
+        function(server_name) -- default handler (optional)
+          require("lspconfig")[server_name].setup({
+            capabilities = capabilities,
+          })
+        end,
+
+        zls = function()
+          local lspconfig = require("lspconfig")
+          lspconfig.zls.setup({
+            root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
+            settings = {
+              zls = {
+                enable_inlay_hints = true,
+                enable_snippets = true,
+                warn_style = true,
+              },
+            },
+          })
+          vim.g.zig_fmt_parse_errors = 0
+          vim.g.zig_fmt_autosave = 0
+        end,
+        ["lua_ls"] = function()
+          local lspconfig = require("lspconfig")
+
+          lspconfig.lua_ls.setup({
+            capabilities = capabilities,
+            settings = {
+              Lua = {
+                runtime = {
+                  version = "LuaJIT",
+                },
+                diagnostics = {
+                  globals = { "vim" },
+                },
+                workspace = {
+                  library = vim.api.nvim_get_runtime_file("", true),
+                  checkThirdParty = false,
+                },
+                format = {
+                  enable = true,
+                  -- Put format options here
+                  -- NOTE: the value should be STRING!!
+                  defaultConfig = {
+                    indent_style = "space",
+                    indent_size = "2",
+                  },
+                },
+              },
+            },
+          })
+        end,
+        ["tailwindcss"] = function()
+          local lspconfig = require("lspconfig")
+          lspconfig.tailwindcss.setup({
+            capabilities = capabilities,
+            filetypes = {
+              "html",
+              "css",
+              "scss",
+              "javascript",
+              "javascriptreact",
+              "typescript",
+              "typescriptreact",
+              "vue",
+              "svelte",
+              "heex",
+            },
+          })
+        end,
+      },
+    })
+
+    local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+        ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+      }),
+      sources = cmp.config.sources({
+        { name = "copilot", group_index = 2 },
+        { name = "nvim_lsp" },
+        { name = "luasnip" }, -- For luasnip users.
+      }, {
+        { name = "buffer" },
+      }),
+    })
+
+    vim.diagnostic.config({
+      -- update_in_insert = true,
+      float = {
+        focusable = false,
+        style = "minimal",
+        border = "rounded",
+        source = "always",
+        header = "",
+        prefix = "",
+      },
+    })
   end,
 }
