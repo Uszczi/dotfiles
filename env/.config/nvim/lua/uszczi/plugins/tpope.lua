fe14fbbffc5812a93814cdea77e0df9d7aabad81
Mateusz Papuga <mateuszpapuga24@gmail.com>
2026-03-09 18:55:16 -0600

    add close fugitive remap

--- env/.config/nvim/lua/uszczi/plugins/tpope.lua
+++ env/.config/nvim/lua/uszczi/plugins/tpope.lua
@@ -7,5 +7,22 @@ return {
     "tpope/vim-sleuth",
   },
 
-  config = function() vim.keymap.set("n", "<Space>gg", ":Git<CR>") end,
+  config = function()
+    vim.keymap.set("n", "<Space>gg", ":Git<CR>")
+
+    local uszczi_augroup = vim.api.nvim_create_augroup("Uszczi_Fugitive", {})
+
+    vim.api.nvim_create_autocmd({ "BufEnter" }, {
+      group = uszczi_augroup,
+      pattern = { "*" },
+      callback = function()
+        if vim.bo.ft ~= "fugitive" then return end
+
+        local buffnr = vim.api.nvim_get_current_buf()
+        local opts = { buffer = buffnr }
+
+        vim.keymap.set({ "n" }, "q", ":q<CR>", opts)
+      end,
+    })
+  end,
 }
