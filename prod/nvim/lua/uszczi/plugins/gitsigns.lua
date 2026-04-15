3f875a00dd9a2ee9b73124a06bf03f8207ff7ff3
Roalec <roalec@o2.pl>
2026-04-15 15:10:18 +0200

    update gitsigns

--- prod/nvim/lua/uszczi/plugins/gitsigns.lua
+++ prod/nvim/lua/uszczi/plugins/gitsigns.lua
@@ -4,13 +4,24 @@ return {
   opts = {},
   keys = {
     {
+      "[c",
+      "<cmd>Gitsigns prev_hunk<CR>",
+      desc = "Jump to previous hunk",
+    },
+    {
+      "]c",
+      "<cmd>Gitsigns next_hunk<CR>",
+      desc = "Jump to next hunk",
+    },
+    {
+      "<leader>gs",
+      "<cmd>Gitsigns stage_hunk<CR>",
+      desc = "Stage hunk",
+    },
+    {
       "<leader>go",
       "<cmd>Gitsigns preview_hunk_inline<CR>",
       desc = "Hunk Diff inline",
-    },
-    {
-      "<leagO",
-      desc = "Hunk Diff",
     },
     {
       "<leader>gu",
